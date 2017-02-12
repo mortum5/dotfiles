c4a82387ba692a0b491a8163178b6ec3d4991f39
mortum5 <jeron312@gmail.com>
2017-02-12 21:46:33 +0300

    add nixos simple config

---
+++ configuration.nix
@@ -0,0 +1,82 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+	
+	boot = {
+		initrd.checkJournalingFS = false;
+		loader.grub = {
+			enable = true;
+			version = 2;
+			device = "/dev/sda";
+		};
+	};
+
+	programs = {
+		zsh.enable = true;
+	};
+	
+	services.xserver = {
+		enable		= true;
+		layout 		= "us,ru";
+		
+		displayManager.lightdm = {
+			enable  = true;
+		};
+		
+		windowManager = {
+			default = "i3-gaps";
+			i3-gaps.enable = true;
+		};
+	};
+	
+	users.extraUsers.andrey = {
+		createHome  = true;
+		home        = "/home/andrey";
+		group       = "users";
+		extraGroups = [ "wheel" "audio"];
+		shell       = "${pkgs.zsh}/bin/zsh";
+		uid         = 1000;
+	};
+
+	time.timeZone = "Europe/Moscow";
+	
+	nixpkgs.config = {
+		allowUnfree = true;
+	};
+
+	environment.systemPackages = with pkgs; [
+		git
+		htop
+		wget
+		zsh
+		vim
+		ranger
+	];
+
+	fonts = {
+		fontconfig.enable = true;
+		enableFontDir = true;
+		enableGhostscriptFonts = true;
+		fontconfig.defaultFonts.monospace = ["Terminus"];
+		fonts = [
+			pkgs.corefonts
+			pkgs.clearlyU
+			pkgs.cm_unicode
+			pkgs.dejavu_fonts
+			pkgs.freefont_ttf
+			pkgs.terminus_font
+		];
+	};
+   
+  virtualisation.virtualbox.guest.enable = true;
+  system.stateVersion = "16.09";
+
+}

3475b03a2c28cab926009a2bed816e03bc52c0f6
Andrey Gonchar <jeron312@gmail.com>
2017-11-06 00:32:32 +0300

    add current nixos config and tilda config; remove old

--- configuration.nix
+++ configuration.nix
@@ -9,81 +9,96 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-	
-	boot = {
-		initrd.checkJournalingFS = false;
-		loader.grub = {
-			enable = true;
-			version = 2;
-			device = "/dev/sda";
-		};
-	};
 
-	programs = {
-		zsh.enable = true;
-	};
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-	hardware.pulseaudio.enable = true;	
+  # networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Enables wireless support via wpa_supplicant.
+  
+  hardware.pulseaudio.enable = true;
+  hardware.bumblebee.enable = true;
+  hardware.bumblebee.connectDisplay = true;
 
-	nix.extraOptions = ''
-		connect-timeout = 10
-	'';
-	
-	services.xserver = {
-		enable		= true;
-		layout 		= "us,ru";
-		xkbOptions      = "grp:ctrl_shift_toggle";
-			
-		displayManager.lightdm = {
-			enable  = true;
-		};
-		
-		windowManager = {
-			default = "i3-gaps";
-			i3-gaps.enable = true;
-		};
-	};
-	
-	users.extraUsers.andrey = {
-		createHome  = true;
-		home        = "/home/andrey";
-		group       = "users";
-		extraGroups = [ "wheel" "audio"];
-		shell       = "${pkgs.zsh}/bin/zsh";
-		uid         = 1000;
-	};
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
 
-	time.timeZone = "Europe/Moscow";
-	
-	nixpkgs.config = {
-		allowUnfree = true;
-	};
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
 
-	environment.systemPackages = with pkgs; [
-		git
-		htop
-		wget
-		zsh
-		vim
-		ranger
-	];
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.firefox.enableAdobeFlash = true;
 
-	fonts = {
-		fontconfig.enable = true;
-		enableFontDir = true;
-		enableGhostscriptFonts = true;
-		fontconfig.defaultFonts.monospace = ["Terminus"];
-		fonts = [
-			pkgs.corefonts
-			pkgs.clearlyU
-			pkgs.cm_unicode
-			pkgs.dejavu_fonts
-			pkgs.freefont_ttf
-			pkgs.terminus_font
-		];
-	};
-   
-  virtualisation.virtualbox.guest.enable = true;
-  system.stateVersion = "16.09";
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    gcc wget vim firefox bash gnumake htop curl git stack
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.bash.enableCompletion = true;
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us,ru";
+  services.xserver.videoDrivers = ["intel"];
+  services.xserver.xkbOptions = "grp:alt_shift_toggle";
+
+  nix.extraOptions = ''
+	connect-timeout = 10
+  '';
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.andrey = {
+    isNormalUser = true;
+    name = "andrey";
+    group = "users";
+    extraGroups = [
+      "wheel" "audio" "video" "networkmanager" "systemd-journal"
+    ];
+    home = "/home/andrey";
+    createHome = false;
+    uid = 1000;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "17.09"; # Did you read the comment?
 
 }
